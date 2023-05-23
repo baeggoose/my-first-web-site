c9635cde7695a122f993f945d589934e09e8935e
baeggoose <bsj154@gmail.com>
2023-05-23 23:23:36 +0900

    JS image gallery 이미지 선택 기능 만들기_onclick 이미지 선택해제 추가

--- JSandWebFE/script.js
+++ JSandWebFE/script.js
@@ -8,7 +8,11 @@ req.onreadystatechange = function () {
       var div = document.createElement("div");
       div.setAttribute("class", "image");
       div.onclick = function () {
-        this.setAttribute("class", "image image-selected");
+        if (this.getAttribute("class").indexOf("image-selected") == -1) {
+          this.setAttribute("class", "image image-selected");
+        } else {
+          this.setAttribute("class", "image");
+        }
       };
       var img = document.createElement("img");
       img.src = data[i];
